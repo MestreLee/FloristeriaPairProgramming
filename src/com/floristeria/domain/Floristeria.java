516f711fc4be33d66ffe7446b3151cae153d0094
Andreu Vinyoles <andreu_vinyoles@hotmail.com>
2021-07-13 11:33:35 +0200

    classe floristeria creada

--- src/com/floristeria/domain/Floristeria.java
+++ src/com/floristeria/domain/Floristeria.java
@@ -1,5 +1,32 @@
 package com.floristeria.domain;
 
 public class Floristeria {
+	private String nom;
+	
+	
+	
+	//crea una floristeria i li assigna un nom.
+	public Floristeria crearFloristeria(String nom) {
+		
+		Floristeria newFloristeria = new Floristeria();
+		
+		newFloristeria.setNom(nom);
+		
+		return newFloristeria;
+	}
+	
+	
+	
+	
+	//getters & setters
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
 
 }
